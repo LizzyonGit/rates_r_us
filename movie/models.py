01c0e801ec7affc593524d7aed6dd480278bfc62
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-04-07 09:54:47 +0200

    Add Post model fields and install django-countries and import field

--- movie/models.py
+++ movie/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+
 
 # Create your models here.
+class Post(models.Model):
+    MOVIE_GENRE = {
+        '1': 'Mystery',
+        '2': 'Comedy',
+        '3': 'Thriller',
+    }
+    movie_title = models.CharField(max_length=200, unique=True)
+    genre = models.CharField(max_length=3, choices=MOVIE_GENRE)
+    country = CountryField()
