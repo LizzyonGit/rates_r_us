b1b7facfadd30713bb250fd185e706cd5e8d8cac
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-04-08 13:45:13 +0200

    Add Genre to admin

--- movie/admin.py
+++ movie/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie, Review, Actor, Director
+from .models import Movie, Review, Actor, Director, Genre
 
 # From https://stackoverflow.com/questions/14828168/django-show-filter-horizontal-on-user-change-admin-page
 # From https://stackoverflow.com/questions/73570167/django-filter-horizontal-how-to-connect-more-fields-together
@@ -17,4 +17,4 @@ admin.site.register(Movie, HorizontalFilter)
 admin.site.register(Review)
 admin.site.register(Actor)
 admin.site.register(Director)
-
+admin.site.register(Genre)
