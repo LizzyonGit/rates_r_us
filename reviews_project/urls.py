f2396ea2109c4a5e90e5409e161fa9c582af6a32
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-04-09 13:07:50 +0200

    Change project name, start add movie list view

--- reviews_project/urls.py
+++ reviews_project/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from movie import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'), 
+    path('', include('movie.urls'), name='movie-urls'), 
     path('admin/', admin.site.urls),
 ]
