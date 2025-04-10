000be8431f6688c04be0a10c34a4700a448e1cb6
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-04-10 13:45:16 +0200

    Add html and css file from CI and start with adjusting it

--- movie/views.py
+++ movie/views.py
@@ -8,6 +8,8 @@ class MovieList(generic.ListView):
     Filters on published movie posts
     """
     queryset = Movie.objects.all().filter(status=1)
+    template_name = "movie/index.html"
+    paginate_by = 4
 
 
     
