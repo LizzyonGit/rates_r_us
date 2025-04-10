40e491d0ceef702b460be71d1ffa7cc1e4de474a
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-04-10 11:11:26 +0200

    Adjust admin Movie model with summernote and other field customisation

--- movie/admin.py
+++ movie/admin.py
@@ -4,29 +4,30 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Movie)
 class MovieAdmin(SummernoteModelAdmin):
+    """
+    Adds displayed fields in list, which fields can be searched, filter and prepopulated slug when writing post
+    filter_horizontal adds an extra filter to the cast, directed_by and genre field so 
+    it is easier to select from large lists    
+    Implements summernote field
 
-    list_display = ('movie_title', 'created_on', 'status')
-    search_fields = ['movie_title', 'cast']
+    """
+
+    list_display = ('movie_title', 'created_on', 'release_date', 'status')
+    search_fields = ['movie_title', 'cast__name']  # cast_name searches on actor names as cast is manytomany field
     list_filter = ('status',)
     prepopulated_fields = {'slug': ('movie_title',)}
-    summernote_fields = ('plot',)
+    filter_horizontal = ['cast', 'directed_by', 'genre']    
+    summernote_fields = ('plot',)  # works when debug is true
+
 
 
 # From https://stackoverflow.com/questions/14828168/django-show-filter-horizontal-on-user-change-admin-page
 # From https://stackoverflow.com/questions/73570167/django-filter-horizontal-how-to-connect-more-fields-together
 
-class HorizontalFilter(admin.ModelAdmin):
-    """
-    Adds an extra filter to the cast, directed_by and genre field
-    """
-    filter_horizontal = ['cast', 'directed_by', 'genre']
-
-
-# should add release date to list display 
+# Search manytotmany: https://stackoverflow.com/questions/51931762/how-can-we-search-many-to-many-field-in-django-admin-search-field
 
 
 # Register your models here.
-# admin.site.register(HorizontalFilter)
 admin.site.register(Review)
 admin.site.register(Actor)
 admin.site.register(Director)
