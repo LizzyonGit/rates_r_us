19990b3b9ed3613ab8f746b6fbb9d3fc18a7a692
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-04-11 09:36:21 +0200

    Install Allauth

--- reviews_project/urls.py
+++ reviews_project/urls.py
@@ -18,9 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('movie.urls'), name='movie-urls'), 
-
-
+    path('', include('movie.urls'), name='movie-urls'),
 ]
