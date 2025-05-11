ed7fd241c31c04217fd677b23c794c874b256daf
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-05-11 15:08:05 +0200

    Fix python warnings

--- movie/models.py
+++ movie/models.py
@@ -39,32 +39,30 @@ class Movie(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     top_pick = models.BooleanField(default=False)
     pick_motivation = models.TextField(max_length=300, null=True, blank=True)
-    
+
     class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
         return self.movie_title
-    
+
     # Two methods for displaying average rating when there are approved reviews
+    # Calculate average rating of approved reviews.
+    # Inspired by https://stackoverflow.com/questions/55325723/generate-average-for-ratings-in-django-models-and-return-with-other-model
+
     def approved_reviews(self):
         return self.reviews.filter(approved=True)
-    
+
     def get_average_rating(self):
-        average_rating = self.approved_reviews().aggregate(Avg('rating')).get('rating__avg')
+        average_rating = (self.approved_reviews().aggregate(Avg('rating'))
+                          .get('rating__avg'))
         return average_rating
-    
-
-        
-# Calculate average rating of approved reviews. Inspired by https://stackoverflow.com/questions/55325723/generate-average-for-ratings-in-django-models-and-return-with-other-model
-
 
 
 class Review(models.Model):
     """
     Description
     """
-     
 
     title = models.CharField(max_length=200, blank=True, null=True)
     author = models.ForeignKey(
@@ -77,25 +75,25 @@ class Review(models.Model):
     movie = models.ForeignKey(
         Movie, on_delete=models.CASCADE, related_name="reviews"
     )
-    # total_likes = IntegerField?
+
     class Meta:
         ordering = ["-created_on"]
+
     def __str__(self):
         return f'{self.title}'
 
-   
 
 class Actor(models.Model):
     """
     Descr
     """
     name = models.CharField()
-    
+
     class Meta:
         ordering = ["name"]
+
     def __str__(self):
         return self.name
-    
 
 
 class Director(models.Model):
@@ -103,9 +101,10 @@ class Director(models.Model):
     Descr
     """
     name = models.CharField()
-    
+
     class Meta:
         ordering = ["name"]
+
     def __str__(self):
         return self.name
 
@@ -115,9 +114,9 @@ class Genre(models.Model):
     Descr
     """
     type = models.CharField()
-    
+
     class Meta:
         ordering = ["type"]
+
     def __str__(self):
         return self.type
-    
--- movie/views.py
+++ movie/views.py
@@ -29,6 +29,7 @@ from .forms import ReviewForm
 #         },
 #         )
 
+
 class MovieList(generic.ListView):
     """
     Filters on published movie posts
@@ -38,26 +39,31 @@ class MovieList(generic.ListView):
     queryset = Movie.objects.all().filter(status=1)
     paginate_by = 3
 
-    # According to https://stackoverflow.com/questions/60560493/django-listview-pagination-when-passing-multiple-objects-in-queryset (post by Esmail Shabayek) and django docs
+    # According to https://stackoverflow.com/questions/60560493/django-listview-pagination-when-passing-multiple-objects-in-queryset
+    # (post by Esmail Shabayek) and django docs
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['top_picks'] = Movie.objects.all().filter(top_pick=True)[:3]  # Picks 3 top picks (From https://stackoverflow.com/questions/48872380/display-multiple-queryset-in-list-view (post by Pran Kumar Sarkar))
+        # Picks 3 top picks (From https://stackoverflow.com/questions/48872380/display-multiple-queryset-in-list-view (post by Pran Kumar Sarkar))
+        context['top_picks'] = Movie.objects.all().filter(top_pick=True)[:3]
         return context
-    
+
 
 # Followed tutorial https://learndjango.com/tutorials/django-search-tutorial and django doc for search functionality
-# Help from https://forum.djangoproject.com/t/find-objects-with-mix-distinct-and-order-by/13010, https://stackoverflow.com/questions/73164250/find-unique-values-in-django/73164902, django doc
+# Help from https://forum.djangoproject.com/t/find-objects-with-mix-distinct-and-order-by/13010,
+# https://stackoverflow.com/questions/73164250/find-unique-values-in-django/73164902, django doc
 class SearchResultsView(generic.ListView):
     """
     Seach page
     """
     model = Movie
     template_name = 'movie/search_results.html'
-    
+
     def get_queryset(self):
         query = self.request.GET.get("q")
-        object_list = Movie.objects.filter(
-            Q(movie_title__icontains=query) | Q(cast__name__icontains=query) | Q(directed_by__name__icontains=query), status=1).order_by('movie_title').distinct('movie_title')
+        object_list = (Movie.objects.filter(
+            Q(movie_title__icontains=query) | Q(cast__name__icontains=query) |
+            Q(directed_by__name__icontains=query),
+            status=1).order_by('movie_title').distinct('movie_title'))
         return object_list
 
 
@@ -78,10 +84,6 @@ def movie_detail(request, slug):
     queryset = Movie.objects.filter(status=1)
     movie = get_object_or_404(queryset, slug=slug)
     reviews = movie.reviews.all().order_by("-created_on")
-    
-    
-    
-
 
     if request.method == "POST":
         review_form = ReviewForm(data=request.POST)
@@ -92,34 +94,35 @@ def movie_detail(request, slug):
         user_reviews = movie.reviews.filter(author=request.user)
 
         if user_reviews:
-            messages.add_message(request, messages.ERROR, 'You have already reviewed this movie.')
+            messages.add_message(request, messages.ERROR,
+                                 'You have already reviewed this movie.')
             return HttpResponseRedirect(reverse('movie_detail', args=[slug]))
 
-
-
-        if review_form.is_valid():            
+        if review_form.is_valid():
             review = review_form.save(commit=False)
             review.author = request.user
             review.movie = movie
-            # Following code sets approved to True if the user only gives a rating, and adapts message to it
+            # Following code sets approved to True if user only gives rating
+            # and adapts message to it
             if not review.text and not review.title:
                 review.approved = True
                 review.save()
-                messages.add_message(
-                request, messages.SUCCESS,
-                'Thank you for your rating')
-                return HttpResponseRedirect(reverse('movie_detail', args=[slug]))  # This updates the page after reviewing, so form dissapears
+                messages.add_message(request, messages.SUCCESS,
+                                     'Thank you for your rating')
+                # This updates the page after reviewing
+                return HttpResponseRedirect(reverse
+                                            ('movie_detail', args=[slug]))
             else:
                 review.save()
-                messages.add_message(
-                request, messages.SUCCESS,
-                'Thank you for your review. It will be published after approval.')
-                return HttpResponseRedirect(reverse('movie_detail', args=[slug]))  # This updates the page after reviewing, so form dissapears
-    
-
+                messages.add_message(request, messages.SUCCESS,
+                                     'Thank you for your review.'
+                                     'It will be published after approval.')
+                # This updates the page after reviewing
+                return HttpResponseRedirect(reverse
+                                            ('movie_detail', args=[slug]))
 
     review_form = ReviewForm()
-    
+
     return render(
         request,
         "movie/movie_detail.html",
@@ -127,15 +130,13 @@ def movie_detail(request, slug):
             "movie": movie,
             "reviews": reviews,
             "review_form": review_form,
-            
-         },
-    )
+            },)
+
 
 def review_edit(request, slug, review_id):
     """
     view to edit reviews
     """
-    
     if request.method == "POST":
 
         queryset = Movie.objects.filter(status=1)
@@ -146,19 +147,24 @@ def review_edit(request, slug, review_id):
         if review_form.is_valid() and review.author == request.user:
             review = review_form.save(commit=False)
             review.movie = movie
-            # Sets approved to True if text and title are empty in the updated review, message is different for both situations
+            # Sets approved to True if text and title are empty
+            # in the updated review, message is different for both situations
             if not review.text and not review.title:
                 review.approved = True
                 review.save()
-                messages.add_message(request, messages.SUCCESS, 'Review updated and published')
+                messages.add_message(request, messages.SUCCESS,
+                                     'Review updated and published')
             else:
                 review.approved = False
                 review.save()
-                messages.add_message(request, messages.SUCCESS, 'Review updated and awaiting approval')
+                messages.add_message(request, messages.SUCCESS,
+                                     'Review updated and awaiting approval')
         else:
-            messages.add_message(request, messages.ERROR, 'Error updating review')
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating review')
 
     return HttpResponseRedirect(reverse('movie_detail', args=[slug]))
+
 
 def review_delete(request, slug, review_id):
     """
@@ -172,10 +178,10 @@ def review_delete(request, slug, review_id):
         review.delete()
         messages.add_message(request, messages.SUCCESS, 'Review deleted')
     else:
-        messages.add_message(request, messages.ERROR, 'You can only delete your own reviews')
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own reviews')
 
     return HttpResponseRedirect(reverse('movie_detail', args=[slug]))
-
 
 
 def my_reviews(request):
@@ -184,23 +190,12 @@ def my_reviews(request):
     """
     queryset = Review.objects.all()
     my_reviews = queryset.filter(author=request.user).order_by("-created_on")
-    
     movies = Movie.objects.filter(status=1)
-    
-    
-    
-
-    
-    # paginate_by = 20
-    # can I link to specific review in movie_detail?
-
     return render(
         request,
         "movie/my_reviews.html",
         {
             "my_reviews": my_reviews,
-            "movies": movies
-            
+            "movies": movies,
          },
     )
-
